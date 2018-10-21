7f7c27a4dca2eb0230c9a456329ce388ff200c26
Cedomir Spalevic <cedomir.spalevic@gmail.com>
2018-10-21 12:39:41 -0500

    fixed post request

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,26 @@
 module.exports = function(app, port, records) {
 
+    //app.use(express.bodyParser());
+
     // start server
     app.listen(port);
     console.log(`Sort-and-Fetch RESTful API server runing on port ${port}\n`);
 
     // accept a record, or json array of records in any of the correct formats
     app.post('/records', (request, response) => {
-        records.AddRecords(request);
-        response.send('Success');
+        var body = '';
+        request.setEncoding('utf8');
+        request.on('data', function(chunk) {
+            body += chunk;
+        }).on('end', function() {
+            try {
+                records.parseJSON(body);
+                response.send('Success');
+            }
+            catch(error) {
+                response.send(error);
+            }
+        });
     });
 
     // returns all records in json format, sorted by gender
